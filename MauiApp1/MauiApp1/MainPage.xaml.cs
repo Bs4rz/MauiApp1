b60e109cc42f168e88ba870c7110cab505389243
bruno <bruno.olibeira902@escola.falsa.com>
2025-02-25 22:28:10 -0300

    Adicionando tela 2

--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -20,6 +20,16 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        private void btnTela2_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewPage2());
+        }
+
+        private void btnVoltar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync( MainPage());
+        }
     }
 
 }
